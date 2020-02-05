1be45e94f80fe15dac2803949a24cc4b9ecaafc9
Yuna <hinamoriyasmina@gmail.com>
2020-02-05 16:15:47 +0100

    first angular workshop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { reverse } from "ramda";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"] // un tableau car on peut en ajouter si besoin
 })
+// le @component décor la classe (lui apporte quelque chose)
 export class AppComponent {
-  title = 'i3-ng-plane';
+  monUrl = "https://google.fr";
+  happy: boolean = false;
+  title = "Angular est superieur a react";
+  mesAnimauxPreferes: Array<string> = ["chat", "lapin"];
+  argent: number = 50;
+  toto: any = {
+    toto: "titi",
+    tata: "toao"
+  };
+
+  constructor() {
+    this.title = reverse("Yuna");
+    this.direCoucou();
+    setInterval(() => {
+      this.happy = !this.happy;
+      this.mesAnimauxPreferes = reverse(this.mesAnimauxPreferes);
+    }, 1000);
+  }
+  direCoucou() {
+    console.log("coucou");
+  }
 }
